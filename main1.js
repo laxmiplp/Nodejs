a15f2fb7b90245df17b73f0d607e23d24a1f9984
laxmiplp <laxmiprasanna.plp@gmail.com>
2025-02-19 23:32:35 +0530

    modified ipaddress0

--- main1.js
+++ main1.js
@@ -6,10 +6,3 @@ const server = http.createServer((req, res) => {
 server.listen('3101', () => {
     console.log('server is running')
 })
-// const server1 = http.createServer((req, res) => {
-//     res.write('Welcome to IP Address')
-//     res.end('')
-// })
-// server1.listen('3101', '192.168.100.5', () => {
-//     console.log('Server running showin an IP Address')
-// })
