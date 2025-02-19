055e557a3e974b78c221a21cb9d9d4deecf2dc2f
laxmiplp <laxmiprasanna.plp@gmail.com>
2025-02-20 00:34:25 +0530

    commented index file

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-console.log('Welcome to NodeJs 1')
-console.log('Welcome to js')
-console.log('Welcome to React')
+// console.log('Welcome to NodeJs 1')
+// console.log('Welcome to js')
+// console.log('Welcome to React')
