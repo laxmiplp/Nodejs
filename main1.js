2f3e16389104ef83067b3d76f3ce703601453087
laxmiplp <laxmiprasanna.plp@gmail.com>
2025-02-19 22:25:39 +0530

    removed ip address for deploying purpose

--- main1.js
+++ main1.js
@@ -1,15 +1,15 @@
 const http = require('http')
-// const server = http.createServer((req, res) => {
-//     res.write('Node js server started')
-//     res.end('response provided')
-// })
-// server.listen('3101', () => {
-//     console.log('server is running')
-// })
-const server1 = http.createServer((req, res) => {
-    res.write('Welcome to IP Address')
-    res.end('')
+const server = http.createServer((req, res) => {
+    res.write('Node js server started')
+    res.end('response provided')
 })
-server1.listen('3102', '192.168.100.5', () => {
-    console.log('Server running showin an IP Address')
+server.listen('3101', () => {
+    console.log('server is running')
 })
+// const server1 = http.createServer((req, res) => {
+//     res.write('Welcome to IP Address')
+//     res.end('')
+// })
+// server1.listen('3101', '192.168.100.5', () => {
+//     console.log('Server running showin an IP Address')
+// })
